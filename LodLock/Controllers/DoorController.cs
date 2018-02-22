03a99eb2536c4dbab5ad92f8524ebd2731b0acd5
Boris Valdman <boris.valdman@live.ru>
2018-02-22 20:07:36 +0300

    Initial commit

---
+++ LodLock/Controllers/DoorController.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LodLock.Controllers
+{
+    [Produces("application/json")]
+    public class DoorController : Controller
+    {
+        [HttpPost]
+        [Route("main")]
+        public IActionResult Main(IFormFile message, [FromHeader]string xSerN, [FromHeader] string xAuthKey)
+        {
+            var bytes = new StreamReader(message.OpenReadStream()).ReadToEnd();
+            return Ok(new { All = bytes });
+        }
+    }
+}
